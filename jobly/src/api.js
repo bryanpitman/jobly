588a84c863fab4565c3618a92c986dde15a84a61
Bryan Pitman <pitman.bryan@gmail.com>
2022-09-09 14:45:36 -0700

    removed console.log

--- jobly/src/api.js
+++ jobly/src/api.js
@@ -30,7 +30,6 @@ class JoblyApi {
     try {
       return (await axios({ url, method, data, params, headers })).data;
     } catch (err) {
-      console.error("API Error:", err.response);
       let message = err.response.data.error.message;
       throw Array.isArray(message) ? message : [message];
     }
